d550cb190306d4e0a132d67527abcd26a8b344d0
DanielXIRPL <87768315+DanielXIRPL@users.noreply.github.com>
2021-11-11 12:49:18 +0700

    Add files via upload

---
+++ lat01.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Belajar PHP";
+
+    echo '<br>';
+
+    echo"Saya Siswa SMK";
+
+    echo '<hl style="background-color:red"> Belajar PHP itu mudah</hl>
+          <p style="background-color:blue"> Saya Suka Belajar program/p>
+    ';
+
+    echo 'saya Belajar'.' PHP'.'<br>';
+
+    echo 'Tahun 2019';
+
+?>
+
