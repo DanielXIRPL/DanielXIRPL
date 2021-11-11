274bf397868bc8f703f7b47015dd79096bf46164
DanielXIRPL <87768315+DanielXIRPL@users.noreply.github.com>
2021-11-11 12:47:58 +0700

    Add files via upload

---
+++ lat04.php
@@ -0,0 +1,52 @@
+<?php
+
+    // OPERATOR MATEMATIKA
+    
+    $a = 5;
+    $b = 5;
+
+    $c = $a + $b;
+
+    echo $c;
+
+    $c = $a - $b;
+    echo $c;
+
+    $c - $a * $b;
+    echo $c.'<br>';
+
+    $c = $a / $b;
+    echo floor($c).'<br>';
+
+    $c = $a % $b;
+    echo $c;
+
+    // OPERATOR LOGIKA
+
+    $c = $a < $b;
+    echo $c;
+
+    $c = $a > $b;
+    echo $c;
+
+    $c = $a == $b;
+    echo $c;
+
+    $c = $a != $b;
+    echo $c;
+
+    // INCREMENT
+    $a++;
+    echo $a.'<br>';
+
+    // OPERATOR STRING
+    
+    $kata = 'Sura';
+    $kota = 'baya';
+
+    $hasil = $kata.$kota;
+
+    $hasil .=' KOTA PAHLAWAN';
+    echo $hasil;
+
+?>
---
+++ lat05.php
@@ -0,0 +1,28 @@
+<?php
+
+    $tanggal = 0;
+
+    // $hasil = $tanggal < 32;
+
+    // if ($tanggal < 32) {
+    //      if ($tanggal > 0) {
+    //           echo 'benar';
+    //       }else {
+    //           echo 'salah';
+    //       }  
+    //    }else {
+    //        echo 'salah';
+    //}
+
+    $nilai = 100;
+
+    if ($nilai <= 100) {
+        echo 'Nilai Besar';
+    }else {
+        echo 'nilai salah';
+    }
+
+
+
+
+?>
